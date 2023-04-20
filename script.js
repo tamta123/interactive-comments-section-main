86ebb28ad1dd6c22aebf88f33c44b6a83543435b
tamta123 <tamtag186@gmail.com>
2023-04-20 19:56:23 +0400

    added reply section via script.js

--- script.js
+++ script.js
@@ -26,12 +26,13 @@ const createDomElement = (tag, className, src, text, id, event, eventFc) => {
 for (let index = 0; index < data.comments.length; index++){
 const{ id, content, createdAt, score, user,username,replies } = data.comments[index];
 
-const userContainer = createDomElement('div', 'user-container' )
 const userBox = createDomElement('div', 'user-box');
 const userImage = createDomElement('img', 'user-image', user.image.png);
 const userName = createDomElement('p', 'user-name', null, user.username);
 const commentBox = createDomElement('div', 'comment-box')
 const commentText = createDomElement('p', 'comment-text', null, content, id)
+const scoreReply = createDomElement('div', 'score-reply')
+const replyDiv = createDomElement('div', "reply-div")
 const scoreElement = createDomElement('div', 'score-value', null, score, id )
 const userDate = createDomElement('div', 'user-date')
 const commentDate = createDomElement('p', 'date', null, createdAt )
@@ -45,13 +46,37 @@ minusIcon.src = './images/icon-minus.svg';
 minusIcon.classList.add('icon');
 scoreElement.prepend(plusIcon);
 scoreElement.appendChild(minusIcon);
+const replyIcon = document.createElement('img');
+replyIcon.src = './images/icon-reply.svg'
+const replyComment = document.createElement('p')
+replyComment.textContent='reply'
+ 
 
-commentBox.append(userDate,commentText,scoreElement);
+
+commentBox.append(userDate,commentText,scoreReply);
+scoreReply.append(scoreElement,replyDiv)
+replyDiv.append(replyIcon,replyComment)
 commentSection.append(commentBox )
 userBox.append(userImage, userName);
 userDate.append(userBox, commentDate)
-}
 
-// if (replies !== undefined) {
-//     for (let j = 0; j < replies.length; j++) {
-//         const { id, content, createdAt, score, user, username } = replies[j];
+
+const repliesSection = createDomElement('div', 'replies-section');
+    for (let j = 0; j < replies.length; j++) {
+        const { id, content, createdAt, score, user, username } = replies[j];
+    const replyBox = createDomElement('div', 'reply-box');
+    const replyText = createDomElement('div', 'comment-text', null, content)
+    const replyUserDate = createDomElement('div', 'user-date')
+    const replyUserImage = createDomElement('img', 'user-image', user.image.png);
+    const replyUserName = createDomElement('p', 'user-name', null, user.username);
+    const replyDate = createDomElement('p', 'date', null, createdAt )
+
+
+
+
+    replyUserDate.append(replyUserImage,replyUserName,replyDate)
+    replyBox.append( replyUserDate,replyText);
+    repliesSection.append(replyBox);
+    }
+    commentSection.append(repliesSection);
+}
